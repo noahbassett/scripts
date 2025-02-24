20c77bf6dbba6df2225f8bfd8052a0e84f07d428
noahbassett <thisisactuallymyalt2@gmail.com>
2025-02-24 14:26:42 +0100

    Update kiwi.js

--- kiwi.js
+++ kiwi.js
@@ -1,3 +1,7 @@
 document.getElementById("deleteMe").remove()
 
-document.documentElement.style.setProperty("--hon-black", "rgba(0,0,150,1)")
+//document.documentElement.style.setProperty("--hon-black", "rgba(0,0,150,1)")
+
+const img = document.createElement("img")
+img.src = "https://lolct.vercel.app/image.png"
+document.body.appendChild(img)
